99e79c5d3cf2a5918c9bbc2095577bbe3d181eed
Michael Bylstra <mbylstra@gmail.com>
2016-10-03 16:17:49 +1100

    now using simonh1000/elm-charts

--- src/Top.elm
+++ src/Top.elm
@@ -2,10 +2,8 @@ module Top exposing (init, view, update)
 
 import Svg exposing (svg)
 import Svg.Attributes exposing (width, height)
-import Chart exposing (Scale, Data)
-import BarChart exposing (barChart, color, width)
-import LineChart exposing (lineChart, color, width)
-import ScatterPlot exposing (scatterPlot, color, size)
+
+import Chart
 
 
 import Html exposing (div, h1, text, p, node)
@@ -27,83 +25,71 @@ init =
 
 type Msg = NoOp
 
--- data : Data msg
--- data =
---   [
---     (1, 22.2, []),
---     (2, 34, []),
---     (3, 56, [BarChart.color "yellowgreen"]),
---     (4, 62, []),
---     (5, 77, [])
---   ]
 
-data : Data msg
-data =
-  List.indexedMap
-    (\index row ->
-      ( toFloat index
-      , row
-        |> Array.fromList
-        |> Array.get 2
-        |> Maybe.withDefault 0
-        |> toFloat
-      , []
-      )
+intToMonth i =
+
+  case (Debug.log "month" i) of
+    1 -> "Jan"
+    2 -> "Feb"
+    3 -> "Mar"
+    4 -> "Apr"
+    5 -> "May"
+    6 -> "Jun"
+    7 -> "Jul"
+    8 -> "Aug"
+    9 -> "Sep"
+    10  -> "Oct"
+    11 -> "Nov"
+    12 -> "Dec"
+    _ -> ""
+
+chartData : List (Float, String)
+chartData =
+  List.map
+    (\row ->
+      let
+        rowArray = Array.fromList row
+        value =
+          rowArray
+            |> Array.get 2
+            |> Maybe.withDefault 0
+            |> toFloat
+        year =
+          rowArray
+            |> Array.get 0
+            |> Maybe.withDefault 0
+            |> toString
+        month =
+          rowArray
+            |> Array.get 1
+            |> Maybe.withDefault 0
+            |> intToMonth
+      in
+        (value, month ++ " " ++ year ++ ":")
     )
     repoCreationsByMonth
-
-
-xScale : Scale
-xScale x =
-  20 + x * 20
-
-
-yScale : Scale
-yScale y =
-  600 - y * 3
-
 
 
 style = """
   body {
     font-family: sans-serif;
     margin: 20px;
+    text-size: 10px;
   }
 """
+
+
+chartView data =
+  -- Chart.vBar data
+  Chart.hBar data
+    -- |> Chart.addValueToLabel
+    |> Chart.title "Total Elm Github repos created by month"
+    |> Chart.toHtml
+  -- Chart.lChart data |> Chart.toHtml
 
 view model =
   div []
     [ node "style" [] [ text style ]
     , h1 [ ] [ text "Elm-alytics" ]
-    , p [ ] [ text "Elm git repos created by month" ]
-    , svg
-      [
-        Svg.Attributes.width "1000",
-        Svg.Attributes.height "600"
-      ]
-      [
-        -- barChart
-        --   [
-        --     BarChart.color "pink",
-        --     BarChart.width "5"
-        --   ]
-        --   { data = data
-        --   , xScale = xScale
-        --   , yScale = yScale
-        --   },
-        lineChart
-          [
-            LineChart.color "#7E94C7"
-          ]
-          { data = data
-          , xScale = (\x -> 20 + x * 15)
-          , yScale = (\y -> 500 - y * 1)
-          }
-        -- scatterPlot
-        --   []
-        --   { data = data
-        --   , xScale = xScale
-        --   , yScale = (\y -> 400 - y * 3)
-        --   }
-      ]
-  ]
+    , chartView chartData
+    ]
